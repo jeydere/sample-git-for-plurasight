f1849d8de08eeaf338a8375a2a133f56ec5e29c9
jeydere <jeydere@hotmail.com>
2024-06-30 15:06:36 -0400

    updated toString

--- src/org/jey/model/User.java
+++ src/org/jey/model/User.java
@@ -11,12 +11,6 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	@Override
-	public String toString() {
-		return "User [name=" + name + "]";
-	}
-	
 	
 
 	public int getAge() {
@@ -26,6 +20,13 @@ public class User {
 	public void setAge(int age) {
 		this.age = age;
 	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + "]";
+	}
+	
+	
 	
 	
 	
